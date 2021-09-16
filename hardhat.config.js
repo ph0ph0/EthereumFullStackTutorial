68c6c7291584862e253a350d7f48c2c280231dc1
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-09-16 22:26:44 +0100

    finished off the greeter contract and the Token contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,14 +16,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+
+const { REACT_APP_ALCHEMY_URL, REACT_APP_METAMASK_PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.4",
-  paths:{
-    artifacts: './src/artifacts'
+  paths: {
+    artifacts: "./src/artifacts",
   },
   networks: {
     hardhat: {
-      chainId: 1337
-    }
-  }
+      chainId: 1337,
+    },
+    ropsten: {
+      url: REACT_APP_ALCHEMY_URL,
+      accounts: [`0x${REACT_APP_METAMASK_PRIVATE_KEY}`],
+    },
+  },
 };
